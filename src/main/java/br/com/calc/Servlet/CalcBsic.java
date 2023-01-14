a9124735f229f6d30db6108c83436911978f77a3
gnixorlando <gabyorlandonascimento@gmail.com>
2023-01-14 18:24:58 -0300

    first commit

---
+++ src/main/java/br/com/calc/Servlet/CalcBsic.java
@@ -0,0 +1,63 @@
+package br.com.calc.Servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class CalcBsic
+ */
+@WebServlet("/CalcBsic")
+public class CalcBsic extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CalcBsic() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		
+		PrintWriter out = response.getWriter();
+		
+		String svalorA = request.getParameter("valorA");
+		String svalorB = request.getParameter("valorB");
+		String sopcao = request.getParameter("opcao");
+		
+		double dvalorA = Double.parseDouble(svalorA);
+		double dvalorB = Double.parseDouble(svalorB);
+		double result = 0;
+		
+		if (sopcao.equals("+")) {
+			result= dvalorA + dvalorB;		
+		} else if (sopcao.equals("-") ) {
+			result= dvalorA - dvalorB;
+		} else if (sopcao.equals("*")) {
+			result=dvalorA*dvalorB;
+		} else if (sopcao.equals("/")) {
+			result = dvalorA / dvalorB;
+		}
+		out.print("Resultado<br>" + result);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
